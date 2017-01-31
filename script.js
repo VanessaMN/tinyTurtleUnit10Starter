fd6ca4cd639a12c6483a8c110f904622dd4c691f
Vanessa Mireles <vanessa.mireles.n@gmail.com>
2017-01-31 23:42:50 +0000

    Geometry

--- script.js
+++ script.js
@@ -11,8 +11,55 @@ forward(50);
 right(90);
 forward(50);
 }
-
+function triangle(){
+right(45);
+forward(35);
+right(90);
+forward(35);
+}
+function house(){
+forward(50);
+right(90);
+forward(50);
+right(90);
+forward(50);
+right(90);
+forward(50);
+right(90);
+forward(50);
+right(45);
+forward(35);
+right(90);
+forward(35);
+}
+function pentagon(){
+forward(35);
+right(72);
+forward(35);
+right(72);
+forward(35);
+right(72);
+forward(35);
+right(72);
+forward(35);
+}
+function hexagon(){
+forward(20);
+right(60);
+forward(20);
+right(60);
+forward(20);
+right(60);
+forward(20);
+right(60);
+forward(20);
+right(60);
+forward(20);
+}
 // Type your function call below
-
-
-stamp();
+//square();
+//stamp();
+//triangle();
+//house();
+//pentagon();
+hexagon();
